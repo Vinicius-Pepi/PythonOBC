62b01746fefb1726f1af31e585217ea7ffc3cbc4
Vinicius Pepi <31309987+Vinicius-Pepi@users.noreply.github.com>
2025-01-14 19:17:23 +0000

    Aula 25, listas

---
+++ 10- listas.py
@@ -0,0 +1,23 @@
+gameFifa = ["Fifa 23", 
+            2022, 
+            90.50, 
+            True]
+print(gameFifa)
+
+gamesList = ["Resident Evil 4", 
+             "Star Wars Jedi Survivor", 
+             "The legend of Zelda", 
+             "Red Dead 2"]
+print(gamesList)
+
+#1- Buscar os dois primeiros itens da lista
+print(gamesList[0:2])
+
+#2- Buscar o ultimo item da lista
+print(gamesList[-1])
+
+#3- Buscar jogos até uma determinada posição
+print(gamesList[0:3])
+
+#4- Buscar jogos de uma posição em diante
+print(gamesList[1:])
