64cd26c5fe5c60b1a289dbe6236fa8bc6b3f25d8
Vinicius Pepi <31309987+Vinicius-Pepi@users.noreply.github.com>
2025-01-17 00:14:53 +0000

    Aula 46

--- exercicio5.py
+++ exercicio5.py
@@ -4,26 +4,46 @@ Exercicio 5:
 -> Escreva uma função Python que receba um string e conte o número de letras maiúsculas e minúsculas destra string
 """
 
+
+def lowerOrUpper(): #Definindo a função
+    phrase = input("Digite uma frase: ")
+    upper = 0
+    lower = 0
+    
+    for char in phrase: #Armazenando os caracteres da string phrase enquanto o loop percorre a string
+        if char.isupper(): #Verificando letras maiúsculas
+            upper += 1
+        elif char.islower(): #Verificando letras minúsculas
+            lower += 1
+    print(f"Letras maiusculas: {upper}\nLetras minusculas: {lower}")
+lowerOrUpper()
+    
 """
 *Lista de números pares e ímpares de uma lista
 -> Escreva uma função Python para imprimir os números pares e ímpares em duas listas separadas para cada uma
 """
-pairList = []
+
+evenList = []
 oddList = []
-def pair(num):
-    if num % 2 == 0:
-        print(f"O número {num} é par\n")
-        pairList.append(num)
-    else:
-        print(f"O número {num} é ímpar\n")
-        oddList.append(num)
-    choice = input("Deseja incluir outro número? (y/n)\n")
-    if choice == "y":
-        num = int(input("Escreva um número: "))
-        pair(num)
 
-num = int(input("Escreva um número: "))
-pair(num)
-
-print(f"Lista de números pares:\n{pairList}\nLista de números ímpares\n{oddList}")
-
+def evenOrOdd():    #Definindo a função
+    while True: #O Loop continua até encontrar o "break"
+        try: #Uso do try para verificar se o usuário usará o input corretamente
+            num = int(input("Digite um número: "))
+            if num % 2 == 0: #Verificando se o número é par
+                evenList.append(num) #Adicionando a variável num na lista par
+                print(f"O número {num} é par")
+            else:
+                oddList.append(num) #Adicionando a variável num na lista ímpar
+                print(f"O número {num} é ímpar")
+        except ValueError: #Caso o usuário use o input incorretamente
+            print("Valor inválido, tente novamente")
+            continue
+            
+        choice = input("Deseja adicionar mais um número? (s/n)").lower()
+        if choice != "s":
+            break #Fim da repetição
+        
+evenOrOdd()
+print(f"Lista de números pares:\n{evenList}")
+print(f"Lista de números ímpares:\n{oddList}")
