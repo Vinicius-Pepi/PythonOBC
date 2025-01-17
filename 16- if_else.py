2f2e25ad02c5cef3c58882a9d005479b720776e2
Vinicius Pepi <31309987+Vinicius-Pepi@users.noreply.github.com>
2025-01-17 00:50:20 +0000

    Isolando Exercício 05

--- 16- if_else.py
+++
@@ -1,9 +0,0 @@
-name = input("Digite o nome do jogo: \n")
-yearLaunch = int(input("Digite o ano de lançamento do jogo: \n"))
-classification = float(input("Digite a nota de classificação do jogo: \n"))
-
-if classification > 8.0 or yearLaunch > 2015:
-    print(f"O jogo {name} é bom. Recomendo jogá-lo")    
-    
-else:
-    print(f"O jogo {name} não é bom")
